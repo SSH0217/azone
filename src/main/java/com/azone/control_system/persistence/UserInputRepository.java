80b34a55ad795bb5ac738a9e578602fe7c427115
SSH0217 <74541304+SSH0217@users.noreply.github.com>
2022-11-07 14:51:48 +0900

    UserInput Statistics

--- src/main/java/com/azone/control_system/persistence/UserInputRepository.java
+++ src/main/java/com/azone/control_system/persistence/UserInputRepository.java
@@ -4,7 +4,20 @@ import com.azone.control_system.model.UserInputEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface UserInputRepository extends JpaRepository<UserInputEntity, String> {
-
+    //고체온
+    List<UserInputEntity> findByTemperatureGreaterThanEqual(double temperature);
+    //저체온
+    List<UserInputEntity> findByTemperatureLessThanEqual(double temperature);
+    //고혈압
+    List<UserInputEntity> findBySbpGreaterThanEqualOrDbpGreaterThanEqual(int sbp, int dbp);
+    //저혈압
+    List<UserInputEntity> findBySbpLessThanEqualOrDbpLessThanEqual(int sbp, int dbp);
+    //서맥
+    List<UserInputEntity> findByPulseLessThanEqual(int pulse);
+    //빈맥
+    List<UserInputEntity> findByPulseGreaterThanEqual(int pulse);
 }
--- src/main/java/com/azone/control_system/service/UserInputService.java
+++ src/main/java/com/azone/control_system/service/UserInputService.java
@@ -1,4 +1,37 @@
 package com.azone.control_system.service;
 
+import com.azone.control_system.model.UserInputEntity;
+import com.azone.control_system.persistence.UserInputRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
 public class UserInputService {
+    @Autowired
+    private UserInputRepository repository;
+
+    public int countHighTemperature(){
+        List<UserInputEntity> users = repository.findByTemperatureGreaterThanEqual(37.3);
+        return users.size();
+    }
+    public int countHypothermia(){
+        List<UserInputEntity> users = repository.findByTemperatureLessThanEqual(35);
+        return users.size();
+    }
+    public int countHighBloodPressure(){
+        List<UserInputEntity> users = repository.findBySbpGreaterThanEqualOrDbpGreaterThanEqual(140, 90);
+        return users.size();
+    }
+    public int countHypotension(){
+        List<UserInputEntity> users = repository.findBySbpLessThanEqualOrDbpLessThanEqual(100, 60);
+        return users.size();
+    }
+    public int countBradycardia(){
+        List<UserInputEntity> users = repository.findByPulseLessThanEqual(60);
+        return users.size();
+    }
+    public int countTachycardia(){
+        List<UserInputEntity> users = repository.findByPulseGreaterThanEqual(100);
+        return users.size();
+    }
 }
